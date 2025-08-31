f0536ea212b7f9f76d1597858b800b3b05b8697a
AkashSC <asceng1989@yahoo.com>
2025-08-31 12:36:46 +0530

    Update image_checks.py

--- utils/image_checks.py
+++ utils/image_checks.py
@@ -1,10 +1,19 @@
-import numpy as np
 from PIL import Image
+from io import BytesIO
+import numpy as np
 
-def analyze_image(image_path, logo_template_path):
-    img = Image.open(image_path).convert("RGB")
+def analyze_image(image_file, logo_template_path):
+    """
+    image_file: Streamlit UploadedFile
+    logo_template_path: path to template image
+    """
+    # Read uploaded image as PIL
+    img = Image.open(BytesIO(image_file.read())).convert("RGB")
+    
+    # Read template image
     logo = Image.open(logo_template_path).convert("RGB")
     
+    # Flatten images and compute histogram
     arr_img = np.array(img).ravel()
     arr_logo = np.array(logo).ravel()
     hist_img, _ = np.histogram(arr_img, bins=64, range=(0,255), density=True)
